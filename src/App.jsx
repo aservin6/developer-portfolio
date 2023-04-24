9f01dcbb73c81df02f41ef563a1e277e5700a2af
aservin6 <servin.alejandroii@gmail.com>
2023-04-24 03:38:49 -0500

    updated about section, updated technologies section

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Contact from "./components/Contact";
 const App = () => {
   return (
     <div className="bg-lightBgMobile dark:bg-darkBgMobile bg-fixed bg-no-repeat bg-[length:100%_100%] text-zinc-900 dark:text-zinc-300 min-h-screen font-josefin pt-4 md:bg-lightBgDesktop dark:md:bg-darkBgDesktop bg-zinc-900">
-      <div className="max-w-5xl w-9/12 mx-auto">
+      <div className="max-w-5xl w-9/12 mx-auto md:w-10/12 lg:w-9/12">
         <ThemeSwitch />
         <Intro />
         <Portfolio />
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,6 +5,8 @@ import {
   SiVite,
   SiNpm,
   SiGit,
+  SiNextdotjs,
+  SiVisualstudiocode,
 } from "react-icons/si";
 
 const About = () => {
@@ -14,12 +16,7 @@ const About = () => {
         About
       </h2>
       <p className="text-base font-bold mb-6 md:text-lg">
-        I first learned HTML and CSS in a high school web development class, but
-        it wasn't until late 2021 that I made the decision to become a
-        full-time front-end developer. I started from the beginning and worked
-        my way up, constantly learning and improving my skills. Now, I am
-        proficient in technologies such as React and TailwindCSS and able to
-        build professional front-end applications.
+      As a self-taught front-end developer, I am driven by a passion for creating visually appealing and user-friendly websites. Through self-directed learning, online tutorials, and personal projects, I have honed my skills in HTML, CSS, and JavaScript. I have experience working with frameworks such as React, NextJS, and TailwindCSS, as well as tools such as Git, npm, and Vite.
       </p>
       <h3 className="text-base font-bold md:text-lg mb-5">
         The technologies I use regularly:
@@ -30,13 +27,18 @@ const About = () => {
           React
         </li>
         <li className="flex items-center gap-2">
-          <SiJavascript className="h-5 w-5 md:h-6 md:w-6 text-[#fcdc00]" />
-          Javascript (ES6+)
+          <SiNextdotjs className="h-5 w-5 md:h-6 md:w-6 dark:text-[#fff] text-[#000]" />
+          NextJS
         </li>
         <li className="flex items-center gap-2">
           <SiTailwindcss className="h-5 w-5 md:h-6 md:w-6 text-[#0ea5e9]" />
           TailwindCSS
         </li>
+        <li className="flex items-center gap-2">
+          <SiJavascript className="h-5 w-5 md:h-6 md:w-6 text-[#fcdc00]" />
+          Javascript (ES6+)
+        </li>
+        
         <li className="flex items-center gap-2">
           <SiVite className="h-5 w-5 md:h-6 md:w-6 text-[#646cff]" />
           Vite
@@ -48,6 +50,11 @@ const About = () => {
         <li className="flex items-center gap-2">
           <SiGit className="h-5 w-5 md:h-6 md:w-6 text-[#f14e32]" />
           Git
+        </li>
+        
+        <li className="flex items-center gap-2">
+          <SiVisualstudiocode className="h-5 w-5 md:h-6 md:w-6 text-[#20A8F2]" />
+          VSCode
         </li>
       </ul>
     </div>
